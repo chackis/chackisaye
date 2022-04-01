7d78491562593482e7d46c1c21d92a5de6e42d99
chackis <101972457+chackis@users.noreply.github.com>
2022-04-01 21:35:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age, grade, clas, status):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.clas = clas
+        self.status = status
+    def info():
+        print("Name of the student:" + self.name + "Age:" + str(self.age),
+    max = Student("Max", 13, 10,  True)
+    ruslan = Student("Ruslan", 14, 10, True)
